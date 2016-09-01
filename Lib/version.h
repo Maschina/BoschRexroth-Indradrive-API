f4bcf3bdab1558d13df109b8e66e0e7440cb2550
Robert Hartmann <robert.hartmann@infineon.com>
2016-09-01 19:06:43 +0200

    Move back and file structure clean-up

--- Lib/version.h
+++ Lib/version.h
@@ -5,10 +5,17 @@
 
 // Will be automatically increased by AutoIncreaseBuildNrs.exe
 // cmd: AutoIncreaseBuildNrs.exe version.h
+<<<<<<< HEAD:Lib/API/version.h
 #define VER_BUILDNR 16
 // Will be automatically increased by get-git-rev-HEAD.py
 // cmd: python get-git-rev-HEAD.py $(MSBuildProjectDir) version.h
 #define VER_BUILD					68aad79
+=======
+#define VER_BUILDNR 1
+// Will be automatically increased by get-git-rev-HEAD.py
+// cmd: python get-git-rev-HEAD.py $(MSBuildProjectDir) version.h
+#define VER_BUILD					e694cca
+>>>>>>> parent of 68aad79... Move files from Lib to Lib/API:Lib/version.h
 
 
 #define VERSION_MAJOR				0
