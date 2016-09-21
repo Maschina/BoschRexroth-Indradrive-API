c055d2273ab2db22ebd78d824ec895225843e053
Robert Hartmann <robert.hartmann@infineon.com>
2016-09-21 16:29:33 +0200

    Adapt new functions to Python app

--- apps/PythonApplication1/PythonApplication1.py
+++ apps/PythonApplication1/PythonApplication1.py
@@ -1,13 +1,14 @@
 import sys
 import ctypes
 from ctypes import cdll
+import os
 
 # Minimum Python 3.3 required
 assert sys.version_info >= (3,3)
 
 
 # Load Indradrive API DLL into memory (use absolute or relative path for 'libpath')
-libpath = "C:\\AE\\Alternator_Testbench\\Software\\API-Hardware\\Indradrive\\Lib\\IndradriveAPI.dll"
+libpath = os.path.dirname(__file__) + "\\..\\..\\bin\\IndradriveAPI.dll"
 indralib = cdll.LoadLibrary(libpath)
 
 # Error-specific class
@@ -18,9 +19,15 @@ class ERR(ctypes.Structure):
         return str(self.msg, "UTF-8")
 
 indra_error = ERR(0)
-indra_error_call = ctypes.byref(indra_error)
-indra_error_out = indra_error.get_msg_str
 
+
+def check_result(result):
+    if result: 
+        print("Error occurred: " + indra_error.get_msg_str())
+        sys.exit(result)
+
+def get_bit(byteval, idx):
+    return ((byteval&(1<<idx))!=0);
 
 
 # MAIN ENTRY POINT
@@ -29,59 +36,65 @@ def main():
     indraref = indralib.init()
 
     # Opening communication channel
-    result = indralib.open(indraref, b"COM1", 19200, indra_error_call)
-    if result: 
-        print("Error occurred: " + indra_error_out())
-        return result
+    result = indralib.open(indraref, b"COM1", 19200, ctypes.byref(indra_error))
+    check_result(result)
 
 
 
-    # Check Operation Mode
-    opmode = ctypes.c_uint32(0)
-    result = indralib.get_opmode(indraref, ctypes.POINTER(opmode), indra_error_call)
-    if result: 
-        print("Error occurred: " + indra_error_out())
-        return result
+    # 
+    # Check Drive Mode
+    # 
+    drvmode = ctypes.c_uint32(0)
+    result = indralib.get_drivemode(indraref, ctypes.byref(drvmode), ctypes.byref(indra_error))
+    check_result(result)
 
-    if opmode != 2: # Operation Mode is not "Speed Control" -> Change it
+    if drvmode.value != 2: # Drive Mode is not "Speed Control" -> Change it
         input("Please make sure to DISABLE the drive release before continue (stand-by mode)!\n(Press any key to continue...)")
 
         # Activate Speed Control
-        result = indralib.speedcontrol_activate(indraref, indra_error_call)
-        if result: 
-            print("Error occurred: " + indra_error_out())
-            return result
+        result = indralib.speedcontrol_activate(indraref, ctypes.byref(indra_error))
+        check_result(result) 
+            
+    for i in range(5):
+        speed = ctypes.c_int32(0)
+        result = indralib.get_speed(indraref, ctypes.byref(speed), ctypes.byref(indra_error))
+        check_result(result)
+        print(speed.value, "rpm")
 
-        input("Please make sure to RELEASE before continue (torque-controlled operation mode)!\n(Press any key to continue...)")
-    
+
+    # 
+    # Check Operation State
+    # 
+    while True:
+        opstate = ctypes.c_uint8(0)
+        result = indralib.get_opstate(indraref, ctypes.byref(opstate), ctypes.byref(indra_error))
+        check_result(result)
+
+        if (opstate.value & 0b11) != 0b11:
+            input("Please make sure to RELEASE before continue (torque-controlled operation mode)!\n(Press any key to continue...)")
+        else:
+            break
+
 
     # Set limits
-    result = indralib.speedcontrol_init(indraref, 10000, 1000, indra_error_call)
-    if result: 
-        print("Error occurred: " + indra_error_out())
-        return result
+    result = indralib.speedcontrol_init(indraref, 10000, 1000, ctypes.byref(indra_error))
+    check_result(result)
 
     # Set speed
-    result = indralib.speedcontrol_write(indraref, 100, 10000, indra_error_call)
-    if result: 
-        print("Error occurred: " + indra_error_out())
-        return result
+    result = indralib.speedcontrol_write(indraref, 100, 10000, ctypes.byref(indra_error))
+    check_result(result)
 
     input("(Press any key to stop the drive...)")
 
     # Clear speed
-    result = indralib.speedcontrol_write(indraref, 0, 10000, indra_error_call)
-    if result: 
-        print("Error occurred: " + indra_error_out())
-        return result
+    result = indralib.speedcontrol_write(indraref, 0, 10000, ctypes.byref(indra_error))
+    check_result(result)
 
 
 
     # Closing communication channel
-    result = indralib.close(indraref, indra_error_call)
-    if result: 
-        print("Error occurred: " + indra_error_out())
-        return result
+    result = indralib.close(indraref, ctypes.byref(indra_error))
+    check_result(result)
 
     return 0
 
