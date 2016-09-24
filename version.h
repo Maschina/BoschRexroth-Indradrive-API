8f0c7d37c101b1562101733bea3bc49cddf1e1b3
Robert Hartmann <robert.hartmann@infineon.com>
2016-09-24 02:02:08 +0200

    Version update

--- version.h
+++ version.h
@@ -5,14 +5,14 @@
 
 // Will be automatically increased by AutoIncreaseBuildNrs.exe
 // cmd: AutoIncreaseBuildNrs.exe version.h
-#define VER_BUILDNR 116
+#define VER_BUILDNR 2
 // Will be automatically increased by get-git-rev-HEAD.py
 // cmd: python get-git-rev-HEAD.py $(MSBuildProjectDir) version.h
-#define VER_BUILD					c03af51
+#define VER_BUILD					396aec7
 
 
 #define VERSION_MAJOR				0
-#define VERSION_MINOR				4
+#define VERSION_MINOR				5
 #define VERSION_REVISION			VER_BUILDNR
 #define VERSION_BUILD				VER_BUILD
 
